0ebb28229094604d3e4f0477c0d21213860fb09b
Arthur <Arthur@cliente-200-135-78-2.wireless.ufsc.br>
2017-03-27 16:22:38 -0300

    teste commit 1

--- src/banco/de/sangue/BancoDeSangue.java
+++ src/banco/de/sangue/BancoDeSangue.java
@@ -15,7 +15,7 @@ public class BancoDeSangue {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+            System.out.println("Teste commit");
     }
     
 }
